6c3ce3496a8ebafb24dee51ec2a022c8f4835d80
zainulabideen <z.abideen@bayt.net>
2024-12-15 21:51:18 +0500

    file for swagger

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Documentation",
+ *      description="API documentation for the Laravel application",
+ *      @OA\Contact(
+ *          email="support@example.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+class SwaggerController
+{
+    // This controller is for Swagger annotations only
+}
